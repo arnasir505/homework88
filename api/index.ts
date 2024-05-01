baea6e6b9626f2679cd974d5efef477cacd9aed6
Ariet Nasirdinov <ariet.nasirdinov505@gmail.com>
2024-05-01 19:25:55 +0600

    Create express app

---
+++ api/index.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+import cors from 'cors';
+import mongoose from 'mongoose';
+import config from './config';
+
+const app = express();
+const port = 8000;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static('public'));
+app.use(express.static('fixtures'));
+
+const run = async () => {
+  await mongoose.connect(config.mongoose.db);
+
+  app.listen(port, () => {
+    console.log(`Server started at http://localhost:${port}`);
+  });
+
+  process.on('exit', () => {
+    mongoose.disconnect();
+  });
+};
+
+void run();
