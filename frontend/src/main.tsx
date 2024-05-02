9386682a7ba3b905a00a7f0ecfdc3e8171a659e0
Ariet Nasirdinov <ariet.nasirdinov505@gmail.com>
2024-05-02 15:12:22 +0600

    Create mui theme, app hooks and redux store with redux-persist

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,21 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { BrowserRouter } from 'react-router-dom';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import theme from './theme';
+import { persistor, store } from './app/store';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <PersistGate persistor={persistor}>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <App />
+        </ThemeProvider>
+      </BrowserRouter>
+    </PersistGate>
+  </Provider>
 );
